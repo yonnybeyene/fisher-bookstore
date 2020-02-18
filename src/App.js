b668e2e4611c65504fbe58e4b4bb959fe262234a
Yonny Beyene <YonnyB@Yonnys-MacBook-Pro.local>
2020-02-18 13:42:24 -0500

    add files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+//import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App Container">
-        <Navbar bg="light" expand="lg">
-          <Navbar.brand>
+        <Navbar bg="dark" expand="lg">
+          <Navbar.Brand>
             <Link to="/">Fisher Bookstore</Link>
-          </Navbar.brand>
+          </Navbar.Brand>
         </Navbar>
       </div>
     );
