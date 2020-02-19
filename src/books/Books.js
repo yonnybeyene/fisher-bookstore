dc0e9825217ce034e0d8c8969aac64dea3ee17a1
yonnybeyene <beyene.13@osu.edu>
2020-02-19 16:40:02 -0500

    Book feature (#19)
    * add files
    
    * add file
    
    * add file

---
+++ src/books/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Books.css";
+
+export default function Books() {
+    return (
+        <div className="Books">
+            <div className="lander">
+                <h1>Books</h1>
+                <p>Welcome to Fisher Bookstore.</p>
+            </div>
+        </div>
+    );
+}
