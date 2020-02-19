97d0756a34e4ca68d44da76980194a3d9dc84f65
Yonny Beyene <YonnyB@Yonnys-MacBook-Pro.local>
2020-02-19 15:31:34 -0500

    add files

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App Container">
-        <Navbar bg="dark" expand="lg">
+        <Navbar bg="light" expand="lg">
           <Navbar.Brand>
             <Link to="/">Fisher Bookstore</Link>
           </Navbar.Brand>
