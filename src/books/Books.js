dda9900f97d12601da4ec23e4cd2b9052b469bf1
Yonny Beyene <YonnyB@Yonnys-MacBook-Pro.local>
2020-02-19 16:38:41 -0500

    add file

--- src/books/Books.js
+++ src/books/Books.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Books.css";
 
-export default function Home() {
+export default function Books() {
     return (
         <div className="Books">
             <div className="lander">
