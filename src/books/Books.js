64466750cda974dc223be8669ce28a187d910762
Yonny Beyene <YonnyB@Yonnys-MacBook-Pro.local>
2020-02-24 13:43:43 -0500

    add files

--- src/books/Books.js
+++ src/books/Books.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Books.css";
+import { BookDisplay } from "./BooksDisplay";
+import { render } from "@testing-library/react";
+
 
 export default function Books() {
-    return (
-        <div className="Books">
-            <div className="lander">
-                <h1>Books</h1>
-                <p>Book Checkout</p>
+
+constructor(props); {
+    super(props);
+    this.state = {
+        books: [
+            {
+            id: 1,
+            title: "Domain Driven Design",
+            author: "Eric Evans",
+            isbn: "978-0321125217"
+            },
+            {
+            id: 2,
+            title: "Accelerate",
+            author: "Nicole Forsgren",
+            isbn: "978-1942788331"
+            }
+        ]
+    };
+    }
+    render(); {
+        return (
+            <div className="Books">
+                <div className="lander">
+                    <BookDisplay books={this.state.books} />
+                </div>
             </div>
-        </div>
     );
+}
 }
