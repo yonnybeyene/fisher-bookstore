dffcc35d60a4e07b2de7da50995e4199c577f1ac
Yonny Beyene <YonnyB@Yonnys-MacBook-Pro.local>
2020-02-26 17:53:35 -0500

    add files

--- src/books/Books.js
+++ src/books/Books.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Books.css";
-import { BookDisplay } from "./BooksDisplay";
+import { BookDisplay } from "./BookDisplay";
 import { render } from "@testing-library/react";
 
 
