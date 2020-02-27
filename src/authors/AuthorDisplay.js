bd1b0bd46edbc8b285f2d1b4a733cb66412bd351
Yonny Beyene <YonnyB@Yonnys-MacBook-Pro.local>
2020-02-26 21:30:01 -0500

    add files

--- src/authors/AuthorDisplay.js
+++ src/authors/AuthorDisplay.js
@@ -1,14 +1,16 @@
 import React from "react";
-//import { BookCard } from "./BookCard";
-//import { CardColumns } from "react-bootstrap";
+import { AuthorCard } from "./AuthorCard";
+import { CardColumns } from "react-bootstrap";
 
 export function AuthorDisplay(props) {
     return (
         <div className="display">
             <h3>Authors</h3>
+            <CardColumns>
                 {props.authors.map(a => (
                     <p>{a.author}</p>
                 ))}
+            </CardColumns> 
         </div>
     );
 }
