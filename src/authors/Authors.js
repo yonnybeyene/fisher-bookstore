3b446bd57a449062ddb2eb6f8753d8a80ef9679e
yonnybeyene <yonnybeyene@github.com>
2020-05-05 22:24:24 -0400

    commit

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,66 +1,16 @@
-import React, { Component } from "react";
-import "./Authors.css";
+import React, { useEffect, useState } from "react";
 import { AuthorDisplay } from "./AuthorDisplay";
 
-export default class Authors extends Component {
+export default function Authors(props) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-         authors: [
-            {
-            id: 1,
-            title: "Domain Driven Design",
-            author: "Eric Evans",
-            isbn: "978-0321125217"
-            },
-            {
-            id: 2,
-            title: "Accelerate",
-            author: "Nicole Forsgren",
-            isbn: "978-1942788331"
-            },
-            {
-            id: 3,
-            title: "The Giver",
-            author: "Lois Lowry",
-            isbn: "978-1942788332"
-            },
-            {
-            id: 4,
-            title: "Contagious",
-            author: "Jonah Berger",
-            isbn: "978-1942788333"
-            },
-            {
-            id: 5,
-            title: "Hooked",
-            author: "Nir Eyal",
-            isbn: "978-1942788334"                
-            },
-            {
-            id: 6,
-            title: "The Alchemist",
-            author: "Paulo Coehlo",
-            isbn: "978-1942788335"
-            },                    
-            {
-            id: 7,
-            title: "The Spook Who Sat By The Door",
-            author: "Sam Greenlee",
-            isbn: "978-1942788336"
-            },
-            {
-            id: 8,
-            title: "Invisible Influence",
-            author: "Jonah Berger",
-            isbn: "978-1942788337"
-            },
-            ]
-        };
-        }
+    const [data, setData] = useState([]);
 
-    render() {
+    useEffect(() => {
+        fetch("https://localhost:5001/api/books")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
         return (
             <div className="Books">
                 <div className="lander">
@@ -68,5 +18,4 @@ export default class Authors extends Component {
                 </div>
             </div>
     );
-}
 }
